5b00b0b4d316ecaea3b14fc833d913351ab6257a
noogen <tom@noogen.net>
2021-05-16 23:50:50 -0500

    update build

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,9 @@ import Home from "../views/Home.vue";
 const history = createWebHistory();
 
 const routes = [
-  { path: "/", component: Home }
+  { path: "/", component: Home },
+  { path: "/image-approval-vite/", component: Home },
+  { path: "/image-approval-vite/dist/", component: Home }
 ];
 
 const router = createRouter({ history, routes });
